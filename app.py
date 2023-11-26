11cf032b171980934134f8fcd77fc32544fe4861
Anukul <copopoco@gmail.com>
2023-11-26 11:52:25 +0530

    add diabetes_predict html page

--- app.py
+++ app.py
@@ -1,73 +1,45 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Jul  9 19:10:22 2023
-
-@author: HP-PC
-"""
-
+from flask import Flask, request, jsonify
 import numpy as np
 import pickle
-import streamlit as st
 
-# loading the saved model
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+# Load the trained model
 loaded_model = pickle.load(open("trained_model.sav", 'rb'))
 
-
-# creating a function for Prediction
-
+# Create a function for prediction
 def diabetes_prediction(input_data):
-    
-
-    # changing the input_data to numpy array
+    # Changing the input_data to a numpy array
     input_data_as_numpy_array = np.asarray(input_data)
 
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    # Reshape the array as we are predicting for one instance
+    input_data_reshaped = input_data_as_numpy_array.reshape(1, -1)
 
     prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
 
-    if (prediction[0] == 0):
-      return 'The person is not diabetic'
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
     else:
-      return 'The person is diabetic'
-  
-    
-  
-def main():
-    
-    
-    # giving a title
-    st.title('Diabetes Prediction Web App')
-    
-    
-    # getting the input data from the user
-    
-    
-    Pregnancies = st.text_input('Number of Pregnancies')
-    Glucose = st.text_input('Glucose Level')
-    BloodPressure = st.text_input('Blood Pressure value')
-    SkinThickness = st.text_input('Skin Thickness value')
-    Insulin = st.text_input('Insulin Level')
-    BMI = st.text_input('BMI value')
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    Age = st.text_input('Age of the Person')
-    
-    
-    # code for Prediction
-    diagnosis = ''
-    
-    # creating a button for Prediction
-    
-    if st.button('Diabetes Test Result'):
-        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-        
-        
-    st.success(diagnosis)
-    
-    
-    
-    
-    
+        return 'The person is diabetic'
+
+# Create a route for prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    try:
+        # Get the input data as a JSON array
+        input_data = request.get_json(force=True)['data']
+
+        # Make predictions
+        result = diabetes_prediction(input_data)
+
+        # Return the result as JSON
+        return jsonify(result=result)
+
+    except Exception as e:
+        return jsonify(error=str(e))
+
 if __name__ == '__main__':
-    main()
+    # Run the Flask app
+    app.run(debug=True)
